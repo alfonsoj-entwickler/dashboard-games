a816ed39f0bfa7c2e8c1504868404e7e00f8c6c0
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2024-03-26 22:00:03 +0100

    [TASK] create main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className="py-5"></header>
+        {children}
+        <footer className="bg-base-200"></footer>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="container mx-auto px-5 sm:px-0">
 
     </main>
   );

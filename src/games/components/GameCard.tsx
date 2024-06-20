99ee70382b6dd28a84ddf99a1491cb09ee9dd855
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2024-06-20 19:52:52 +0200

    [TASK] updated component card

--- src/games/components/GameCard.tsx
+++ src/games/components/GameCard.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import { SimpleGame } from "../interfaces/simple-game";
 import Image from "next/image";
 
@@ -21,7 +22,28 @@ export default function GameCard({ game, img }: Props) {
       />
       <div className="absolute bottom-0 left-0 w-full h-[60%] bg-linear-black z-10" />
       <div className="flex flex-col gap-2 absolute bottom-0 left-0 w-full h-[40%] p-6 text-white z-20">
-        <span className="self-start p-1 text-xs text-white bg-slate-500">PC / XBOX / PS5</span>
+        <ul className="flex">
+          <li>
+            <span className="bg-pc p-1 text-xs text-white">
+              <Link href="/hardware/pc">PC</Link>
+            </span>
+          </li>
+          <li>
+            <span className="bg-nintendo p-1 text-xs text-white">
+            <Link href="/hardware/switch">SWITCH</Link>
+            </span>
+          </li>
+          <li>
+            <span className="bg-xbox p-1 text-xs text-white">
+              <Link href="/hardware/xbox">XBOX</Link>  
+            </span>
+          </li>
+          <li>
+            <span className="bg-playstation p-1 text-xs text-white">
+              <Link href="/hardware/ps5">PS5</Link>
+            </span>
+          </li>
+        </ul>
         <h2 className="text-4xl font-semibold uppercase">Headline</h2>
         <p>Text</p>
       </div>

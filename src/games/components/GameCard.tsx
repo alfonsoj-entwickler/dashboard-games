705ec3ddbdeb7440ed3d1ccc6ff8f8df18321dee
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2024-06-25 20:17:36 +0200

    [TASK] updated component card

--- src/games/components/GameCard.tsx
+++ src/games/components/GameCard.tsx
@@ -21,8 +21,8 @@ export default function GameCard({ game, img }: Props) {
         className="relative z-0"
       />
       <div className="absolute bottom-0 left-0 w-full h-[60%] bg-linear-black z-10" />
-      <div className="flex flex-col gap-2 absolute bottom-0 left-0 w-full h-[40%] p-6 text-white z-20">
-        <ul className="flex">
+      <div className="flex flex-col gap-2 absolute bottom-0 left-0 w-full  p-6 text-white z-20">
+        <ul className="flex overflow-hidden">
           <li>
             <span className="bg-pc p-1 text-xs text-white">
               <Link href="/hardware/pc">PC</Link>
@@ -46,6 +46,10 @@ export default function GameCard({ game, img }: Props) {
         </ul>
         <h2 className="text-4xl font-semibold uppercase">Headline</h2>
         <p>Text</p>
+        <div className="flex justify-end">
+          <Link href={"/"} className="py-1.5 px-4 rounded-md text-sm uppercase text-white transition-colors bg-orange-600 hover:bg-orange-800">More</Link>
+        </div>
+        
       </div>
     </div>
   );

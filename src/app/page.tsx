c6aae833abcafc3a76a9a799dc7a109d9be7d6ef
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2024-06-25 20:24:33 +0200

    [TASK] added 100 components to testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import GameCard from "@/games/components/GameCard";
+
+
+
 export default function Home() {
+  const cards = Array.from(Array(100).keys());
+
   return (
     <main className="container mx-auto p-5">
       <h1 className="mb-6 text-4xl font-bold">Main Content</h1>
       <div className="grid grid-cols-4 gap-4">
-        <GameCard img="/images/dummy_cover_01.jpg" game={{id: "test", name: "test"}} />
-        <GameCard img="/images/dummy_cover_02.jpg" game={{id: "test1", name: "test1"}} />
-        <GameCard img="/images/dummy_cover_03.jpg" game={{id: "test2", name: "test2"}} />
-        <GameCard img="/images/dummy_cover_04.jpg" game={{id: "test3", name: "test3"}} />
+        {cards.map( item => (
+          <GameCard key={`test-${item}`} img="/images/dummy_cover.jpg" game={{id: `test-${item}`, name: `test-${item}`}} />
+        ))}
       </div>
     </main>
   );
